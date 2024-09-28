9b7b81d2a59e216a0d6e7fc49521a4fee363e7d1
Hachounet <hachounet.github@gmx.fr>
2024-09-28 13:09:51 +0200

    MOVE DevHub position

---
+++ src/DevHub.js
@@ -0,0 +1,18 @@
+const responsiveWidth = 768;
+const siteURL = 'http://localhost:3000';
+
+const friendsListURL = 'http://localhost:3000/users/friends';
+const friendsRequestURL = 'http://localhost:3000/friends/request';
+const getFriendsRequestURL = 'http://localhost:3000/friends/requests';
+const userInfosUrl = 'http://localhost:3000/users/';
+const postSearchUrl = 'http://localhost:3000/search';
+
+export {
+  responsiveWidth,
+  siteURL,
+  friendsListURL,
+  userInfosUrl,
+  friendsRequestURL,
+  postSearchUrl,
+  getFriendsRequestURL,
+};
