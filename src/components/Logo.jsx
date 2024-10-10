8062ab7188be357fe817fb802995c46a6e9af28d
Hachounet <hachounet.github@gmx.fr>
2024-10-10 11:27:40 +0200

    FIX Logo src Vercel

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <div className=" self-center h-20 object-cover ">
       <img
-        src="public/logo.png"
+        src="/logo.png"
         className="w-20 h-24 object-cover"
         title="LiveChat Logo"
       ></img>
